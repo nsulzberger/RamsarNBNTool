c9c3d92a2c921862c946456fad93f62e9ef3f300
Nicole Sulzberger <njung@ubiq.ch>
2013-04-17 01:44:10 +0200

    Taxon Search UI Prototyp

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RamsarNBNTool.Global" Language="C#" %>
---
+++ javascript/config.js
@@ -0,0 +1,69 @@
+//configRealEstate.js - Javascript configuration for Real Estate Page
+
+var config = 
+{
+	colors: ['#e02d7f','#886197','#6557f0','#5978ff','#3a8ad4','#28a8c8','#2ec623','#80c802','#d2c101','#e68e04','#e66904', '#d12929'],
+	webmap: "125cba5aed014cc887f4b6e20b7e17f7",
+	locX: -122.391365,
+	locY: 37.781792,
+	items: [
+		{
+			name: "Love Thy Neighborhood",
+			title: "THE FIRST COMMANDMENT OF REAL ESTATE",
+			image: "images/hero/re_0.png",
+			desc: "Finding a home is more than deciding on beds and baths, it&apos;s choosing a community that matches your personality. Broaden your search by finding places you love and discovering more places with similar lifestyle characteristics.",
+			url: "http://coolmaps.esri.com/LoveThyNeighborhood"
+		},
+		{
+			name: "Home is<br/>where the mart is",
+			title: "REAL ESTATE FOR SHOPAHOLICS",
+			image: "images/hero/re_1.png",
+			desc: "Find the shopping coefficient of the neighborhood you want to live in. Using the twenty highest rated stores on Yelp in your neighborhood, this map shows if your new home will meet your passion for shopping."
+		
+		},
+		{
+			name: "Sale of<br/>Two Cities",
+			title: "A COST OF LIVING COMPARISON",
+			image: "images/hero/re_2.png",
+			desc: "The grass is always greener on the other side. But how much more &apos;green&apos; does it take to live in San Francisco versus West Palm Beach? Use this map to compare the cost of living in your dream city.",
+			url: "http://coolmaps.esri.com/CostOfLiving"
+		},
+		{
+			name: "Living<br/>La Vida Local",
+			title: "BUY LOCAL | EAT LOCAL | ENJOY LOCAL",
+			image: "images/hero/re_3.png",
+			desc: "Support local businesses in your community.  Add local shops, restaurants and local places you can enjoy to your day. Since everything is in close proximity, you can walk and as a result you will be helping the planet, your community and yourself.",
+			url: "http://coolmaps.esri.com/LaVidaLocal"
+		},
+		{
+			name: "Economic Development",
+			title: "MAPPING NYC ECONOMIC DEVELOPMENT PROJECTS",
+			image: "images/hero/re_4.png",
+			desc: "View economic development projects in New York City by category and tapestry lifestyle information around a project.",
+            url: "http://coolmaps.esri.com/NYCEDCProjects2"
+		},
+		{
+			name: "Crime Time",
+			title: "FIGHTING CRIME 24x7",
+			image: "images/hero/re_5.png",
+			desc: "In lean economic times it is important for crime-fighting super heroes to understand where and when crimes are happening. This map helps visualize San Francisco crime data by neighborhood, day of week and hour of day to help use crime-fighting resources more efficiently.",
+			url: "http://coolmaps.esri.com/CrimeTime"
+        },
+		{
+		    name: "Growth Spurt",
+		    title: "URBAN GROWTH IN SAN FRANCISCO",
+		    image: "images/hero/re_6.png",
+		    desc: "This map helps visualize urban growth in San Francisco since 2000. The map also displays the change in citizen sentiment for various City services over the same period.",
+		    url: "http://coolmaps.esri.com/UrbanGrowth"
+		}
+		/*{
+		    name: "2012",
+		    title: "BEST PHOTOGRAPHS OF THE YEAR",
+		    image: "images/hero/re_7.png",
+		    desc: "The year 2012 flew by before you could recover from the New Year's party hangover. Fortunately, with help from our friends at Reuters this map lets you relive 2012 with the best pictures of the year.",
+		    url: "http://coolmaps.esri.com/2012Recap"
+		}*/
+	]
+};
+
+
